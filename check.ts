27122791632370ab13a81bb4714cfcec0cbfa1ce
rudranil723 <rudranilshil07@gmail.com>
2026-04-12 12:08:50 +0530

    check.ts deleted

--- check.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-    const msgs = await prisma.chatMessage.findMany({ orderBy: { createdAt: 'desc' }, take: 5 })
-    console.log(msgs)
-}
-main().finally(() => prisma.$disconnect())
